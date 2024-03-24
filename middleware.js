8eb09cd899148c91143bbba15363dd897f0d48e4
Harshad <harshadarvindh@gmail.com>
2024-03-24 14:57:43 -0400

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -12,3 +12,23 @@ export default function middleware(req) {
     },
   });
 }
+
+import { SpeedInsights } from '@vercel/speed-insights/next';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
